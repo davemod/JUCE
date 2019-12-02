bc2a5c029cd1e27982fc49ff5ed0732057dee977
David Hill <david.valentin.hill@gmail.com>
2019-12-02 21:53:40 +0100

    Adding CC Input Values

--- modules/juce_audio_processors/format_types/juce_VST3Common.h
+++ modules/juce_audio_processors/format_types/juce_VST3Common.h
@@ -1347,6 +1347,7 @@ public:
     {
         const auto numEvents = eventList.getEventCount();
 
+        
         for (Steinberg::int32 i = 0; i < numEvents; ++i)
         {
             Steinberg::Vst::Event e;
